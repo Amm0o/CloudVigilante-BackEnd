41241863bbad48dcb66742cb3fff2763062c9878
angelo <angelo0108@live.com.pt>
2023-12-02 17:53:22 +0000

    Added logic to grab performance data parse it and send to frontend

--- routes/sendRealTimePerformance.ts
+++ routes/sendRealTimePerformance.ts
@@ -1,4 +1,5 @@
 import { Pool } from "mysql2/promise";
+import { getPerformanceTelemtry } from "../lib/dbHelpers/getPerformanceTelemtry";
 
 export async function sendPerformanceTelemetry (req: Request, connectionPool: Pool): Promise<Response> {
 
@@ -15,11 +16,26 @@ export async function sendPerformanceTelemetry (req: Request, connectionPool: Po
         return new Response("Method not allowed", { status: 405 });
     }
 
+    // Get paramets from URL
+    const url = new URL(req.url);
+    const urlParams = url.searchParams;
+    const timeFrame: string | null = urlParams.get("timeframe");
+
+    const timeFrameInt: number = Math.max(1, parseInt(timeFrame || "1"));
+
+    console.log("Selected TimeframeINT:", timeFrameInt);
+
+
     try {
         // Get data from db:
         // Create a query to get data from table processes 
-        const [rows] = await connectionPool.query("SELECT * FROM Processes WHERE Timestamp > NOW() - INTERVAL 1 SECOND");
-        return new Response(JSON.stringify(rows), { status: 200, headers: headers });
+        // const [rows] = await connectionPool.query("SELECT * FROM Processes WHERE Timestamp > NOW() - INTERVAL ? HOUR", [timeFrameInt]);
+
+        // Grab the data to send to the front end
+        const processDetails = await getPerformanceTelemtry(connectionPool, timeFrameInt);
+    
+    
+        return new Response(JSON.stringify(processDetails), { status: 200, headers: headers });
 
 
     } catch (error) { 
